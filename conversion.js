60fa8559885aebe4e5d5b23ae7d985562fb94318
sahilsandim <144585376+sahilsandim@users.noreply.github.com>
2023-11-06 14:57:53 +0000

    Completed all objects and datatypes

---
+++ conversion.js
@@ -0,0 +1,70 @@
+// let FirstInteger = 'abc';
+// let SecondInteger = Number (FirstInteger);
+// console.log( FirstInteger)
+// console.log(typeof SecondInteger)
+// // console.log(NaN == NaN)  --> It is denoted in the js that it will throw an option false in condition.
+// // console.log(2 == 2) --> Defined by Js
+// // console.log(44 + undefined) --> Defined to not be a number
+// // console.log(44 + null) --> This will print value 44
+// // const Firstname = "spaghatei";
+// // const Lastname = "Bokachoda";
+// // console.log(Firstname + " " + Lastname)
+// // console.log(`The first name of an given person is   ${Firstname}  Last name of an variable is an  ${Lastname}`) 
+
+
+// const firstApplication = "Iphone";
+// // This is how upper code will work in  Js in background
+// // const firstApplication = new String('Iphone')
+// const LastApplication = "Samsung";
+// console.log(firstApplication + '      ' + LastApplication)
+// console.log(`Let the First name of an variable is ${firstApplication} and last value of an element is an ${LastApplication}`)
+// // ${} --> template literal
+
+
+// // To access Individual character
+// let name = new String('yakka')
+// console.log(name)
+// console.log(name.length)
+// console.log(name.charAt(2))
+// console.log(name.substring(0,2))
+
+// let email = "application@99gmail.com  ";
+// let User2 = "application2";
+// console.log(email.toUpperCase())
+// console.log(User2.trim(                                                                                                                                                                                                              ))
+// console.log(email.replace("99","44"))
+// console.log(email.includes("9"))
+
+
+
+
+// // const User1 = new Number(2.335)
+// // console.log(User1.toFixed(1))
+
+// // const subscribers = 10000;
+// // console.log(subscribers.toLocaleString())
+// const Firstuser = new Number(1.123)
+// console.log(Firstuser.toFixed(2))
+
+// const Applications = 12345;
+// console.log(Applications.toLocaleString())
+
+// // Primitive data types
+// // console.log(Math.random ()*10 + 1)
+// // console.log(Math.floor(Math.random ()*10 + 1))
+
+// console.log(Math.random()*10+1)
+// console.log(Math.floor(99.90)+1)
+
+
+const firstArray = [100,200,300,400,500]
+const NewArray = new Array(100,200,300,400,500)
+console.log(firstArray[3])
+
+
+let name = ["User1"," User2 ","User3"]
+name.push("suiiiii")
+name.pop(l)
+console.log(name)
+// It changes the refrence of an given element
+// data is completed totally
