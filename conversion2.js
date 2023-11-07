f8e9344c8046a745b60946d55d4359142aa8bfd2
sahilsandim <144585376+sahilsandim@users.noreply.github.com>
2023-11-07 12:07:00 +0000

    conversion2-->data types

---
+++ conversion2.js
@@ -0,0 +1,20 @@
+const myArray = [10,20,30,49]
+const NewArray = Array[10,20,30,40,49]
+// console.log(myArray[1]);
+
+
+const items = ["Gulabjam", "Jalebi",false,0]
+// console.log(items.length)
+// items.push("Vadapav")
+// console.log(items)
+// let newItems = items.pop()
+// console.log(items)
+// console.log(items.toString())
+// console.log(items.join("_"))
+console.log(items.slice(1,3))
+console.log(items)
+console.log(items.splice(1,3))
+console.log(items)
+
+
+
